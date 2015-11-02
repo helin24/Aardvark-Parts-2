30ccafe01d31d6c60a4c90d815820cb23e6f4a72
helin24 <helin.shiah@gmail.com>
2015-11-02 01:19:28 -0500

    large implementation but not saving time..

--- week-3/knapsack.py
+++ week-3/knapsack.py
@@ -8,9 +8,13 @@ def knapsack(txt_file):
     index = [] # double array with outer representing obj number and inner representing size of knapsack
 
     for item in range(number_of_items):
+        print item
         line = f.readline().split(' ')
         value, weight = int(line[0]), int(line[1][:-1])
-        index.append([])
+        if item <= 1:
+            index.append([])
+        else:
+            index[1] = []
         for size in range(knapsack_size):
             if item == 0:
                 if size + 1 < weight:
@@ -19,16 +23,22 @@ def knapsack(txt_file):
                     index[item].append(value)
             else:
                 if size + 1 < weight:
-                    index[item].append(index[item-1][size])
+                    index[1].append(index[0][size])
                 else:
                     if size - weight >= 0:
-                        index[item].append(max(index[item - 1][size], value + index[item - 1][size - weight]))
+                        index[1].append(max(index[0][size], value + index[0][size - weight]))
                     else:
-                        index[item].append(max(value, index[item - 1][size]))
+                        index[1].append(max(value, index[0][size]))
+        if item > 0:
+            index[0] = index[1]
 
-    return index[item][size]
+    return index[1][size]
+        
+    
+    # could only examine every 100 of knapsack space?
+    
 
 
 
 
-print knapsack('knapsack1.txt')
+print knapsack('knapsack_big.txt')
